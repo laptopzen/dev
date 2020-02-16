b283a911877e1288f20d6d9817e0a5e55f4d3dad
mzaini30 <muhzaini30@gmail.com>
2020-02-16 17:18:28 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -21,26 +21,27 @@ olah_isi = () => {
 	    if(list_artikel_acak.indexOf(r) === -1) list_artikel_acak.push(r);
 	}
 
-	$('.isi').html('')
-
+	isi_artikel = []
 	for(y of list_artikel_acak){
-		$.get(`https://dev.to/api/articles/${y}`, data => $('.isi').html(data.map(x => `
-			<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
-				<table class="table table-ajaib">
-					<tr>
-						<td>
-							<img src="${x.user.profile_image_90}" alt="">
-						</td>
-						<td>
-							<div class="panel panel-default">
-								<div class="panel-heading">${x.title} ~ <em>by ${x.user.name}</em></div>
-								<div class="panel-body">${x.description ? x.description : kosong()}</div>
-							</div>
-						</td>
-					</tr>
-				</table>
-			</a>`))
+		$.get(`https://dev.to/api/articles/${y}`, data => isi_artikel.push(data))
 	}	
+
+	$('.isi').html(isi_artikel.map(x => `
+		<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
+			<table class="table table-ajaib">
+				<tr>
+					<td>
+						<img src="${x.user.profile_image_90}" alt="">
+					</td>
+					<td>
+						<div class="panel panel-default">
+							<div class="panel-heading">${x.title} ~ <em>by ${x.user.name}</em></div>
+							<div class="panel-body">${x.description ? x.description : kosong()}</div>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</a>`))
 }
 
 olah_isi()
