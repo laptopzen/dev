e3722caca0a9533ca3bdf100e7258744d8de5dda
mzaini30 <muhzaini30@gmail.com>
2020-02-16 16:25:53 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,20 +1,5 @@
 $.ajaxSetup({async: false})
 
-$.get('https://dev.to/api/articles', data => banyak = data[0].id)
-
-mau_diambil = 30
-
-list_artikel_acak = [];
-while(list_artikel_acak.length < mau_diambil){
-    r = Math.floor(Math.random() * banyak) + 1;
-    if(list_artikel_acak.indexOf(r) === -1) list_artikel_acak.push(r);
-}
-
-data_acak = []
-for(x of list_artikel_acak){
-	$.get(`https://dev.to/api/articles/${x}`, data => data_acak.push(data))
-}
-
 baca = [
 	'I think, you can love it',
 	'I was explain this',
@@ -25,19 +10,43 @@ baca = [
 
 kosong = () => baca[Math.floor(Math.random() *  baca.length)]
 
-$('.isi').html(data_acak.map(x => `
-	<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
-		<table class="table table-ajaib">
-			<tr>
-				<td>
-					<img src="${x.user.profile_image_90}" alt="">
-				</td>
-				<td>
-					<div class="panel panel-default">
-						<div class="panel-heading">${x.title} ~ <em>by ${x.user.name}</em></div>
-						<div class="panel-body">${x.description ? x.description : kosong()}</div>
-					</div>
-				</td>
-			</tr>
-		</table>
-	</a>`))
+olah_isi = () => {
+	$.get('https://dev.to/api/articles', data => banyak = data[0].id)
+
+	mau_diambil = 30
+
+	list_artikel_acak = [];
+	while(list_artikel_acak.length < mau_diambil){
+	    r = Math.floor(Math.random() * banyak) + 1;
+	    if(list_artikel_acak.indexOf(r) === -1) list_artikel_acak.push(r);
+	}
+
+	data_acak = []
+	for(x of list_artikel_acak){
+		$.get(`https://dev.to/api/articles/${x}`, data => data_acak.push(data))
+	}	
+
+	$('.isi').html(data_acak.map(x => `
+		<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
+			<table class="table table-ajaib">
+				<tr>
+					<td>
+						<img src="${x.user.profile_image_90}" alt="">
+					</td>
+					<td>
+						<div class="panel panel-default">
+							<div class="panel-heading">${x.title} ~ <em>by ${x.user.name}</em></div>
+							<div class="panel-body">${x.description ? x.description : kosong()}</div>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</a>`))
+}
+
+olah_isi()
+
+$('.navbar-brand').click(() => {
+	$('.isi').html('Loading...')
+	olah_isi()
+})
