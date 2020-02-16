a5d426fb1a4c9573c8298cc99c9f3312be3a2c50
mzaini30 <muhzaini30@gmail.com>
2020-02-16 16:58:44 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ baca = [
 kosong = () => baca[Math.floor(Math.random() *  baca.length)]
 
 olah_isi = () => {
-	ajax = $.get('https://dev.to/api/articles?per_page=1', data => banyak = data[0].id)
+	ambil_json = $.get('https://dev.to/api/articles?per_page=1', data => banyak = data[0].id)
 
 	mau_diambil = 30
 
@@ -24,7 +24,7 @@ olah_isi = () => {
 	$('.isi').html('')
 
 	for(y of list_artikel_acak){
-		ajax = $.get(`https://dev.to/api/articles/${y}`, x => $('.isi').append(`
+		ambil_json = $.get(`https://dev.to/api/articles/${y}`, x => $('.isi').append(`
 			<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
 				<table class="table table-ajaib">
 					<tr>
@@ -45,8 +45,8 @@ olah_isi = () => {
 
 olah_isi()
 
-// $('.navbar-brand').click(() => {
-// 	ajax.abort()
-// 	$('.isi').html('Loading...')
-// 	olah_isi()
-// })
+$('.navbar-brand').click(() => {
+	ambil_json.abort()
+	$('.isi').html('Loading...')
+	olah_isi()
+})
