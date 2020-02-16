11c89a2896f2d0fd4b53316f70091cea7359f0b8
mzaini30 <muhzaini30@gmail.com>
2020-02-16 16:02:04 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,5 +1,33 @@
-$.get('https://dev.to/api/articles', data => {
-	banyak = data[0].id
-})
+$.ajaxSetup({async: false})
 
-console.log(banyak)
+$.get('https://dev.to/api/articles', data => banyak = data[0].id)
+
+mau_diambil = 10
+
+list_artikel_acak = [];
+while(list_artikel_acak.length < mau_diambil){
+    r = Math.floor(Math.random() * banyak) + 1;
+    if(list_artikel_acak.indexOf(r) === -1) list_artikel_acak.push(r);
+}
+
+data_acak = []
+for(x of list_artikel_acak){
+	$.get(`https://dev.to/api/articles/${x}`, data => data_acak.push(data))
+}
+
+$('.isi').html(data_acak.map(x => `
+	<a href="${x.url}" class="anggap-aja-bukan-link" target='_blank'>
+		<table class="table table-ajaib">
+			<tr>
+				<td>
+					<img src="${x.user.profile_image_90}" alt="">
+				</td>
+				<td>
+					<div class="panel panel-default">
+						<div class="panel-heading">${x.title}</div>
+						<div class="panel-body">${x.description}</div>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</a>`))
